dc81a34e18830e21508f012510fa26002d65ec52
BisiDavi <oludavidconnect@gmail.com>
2021-07-29 22:16:19 +0100

    main

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,4 +1,5 @@
 const Admin = require('../models/admin.model');
+const decodeAuthToken = require('../utils/decodeToken').decodeToken;
 
 exports.find = async (req, res) => {
     try {
@@ -23,63 +24,87 @@ exports.findById = async (req, res) => {
 };
 
 exports.create = async (req, res) => {
-    try {
-        const admin = new Admin({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            whatsappNumber: req.body.whatsappNumber,
-            userName: req.body.userName,
-            password: req.body.password,
+    const userRole = decodeAuthToken.role;
+
+    if (userRole === 'superAdmin') {
+        try {
+            const admin = new Admin({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                whatsappNumber: req.body.whatsappNumber,
+                userName: req.body.userName,
+                password: req.body.password,
+            });
+            await admin.save();
+            res.send(admin);
+        } catch (error) {
+            console.log('error', error);
+            res.status(error.status);
+            res.send({ error, message: 'unable to create an admin' });
+        }
+    } else {
+        res.status(401).send({
+            message: 'Not authorized, only a super admin can create an admin',
         });
-        await admin.save();
-        res.send(admin);
-    } catch (error) {
-        console.log('error', error);
-        res.status(error.status);
-        res.send({ error, message: 'unable to create an admin' });
     }
 };
 
 exports.edit = async (req, res) => {
-    try {
-        const admin = await Admin.findById({ _id: req.params.id });
-        if (req.body.firstName) {
-            admin.firstName = req.body.firstName;
+    const userRole = decodeAuthToken.role;
+
+    if (userRole === 'superAdmin') {
+        try {
+            const admin = await Admin.findById({ _id: req.params.id });
+            if (req.body.firstName) {
+                admin.firstName = req.body.firstName;
+            }
+            if (req.body.lastName) {
+                admin.lastName = req.body.lastName;
+            }
+            if (req.body.email) {
+                admin.email = req.body.email;
+            }
+            if (req.body.phoneNumber) {
+                admin.phoneNumber = req.body.phoneNumber;
+            }
+            if (req.body.whatsappNumber) {
+                admin.whatsappNumber = req.body.whatsappNumber;
+            }
+            if (req.body.userName) {
+                admin.userName = req.body.userName;
+            }
+            if (req.body.password) {
+                admin.password = req.body.password;
+            }
+        } catch (error) {
+            console.log('error', error);
+            res.status(error.status);
+            res.send({ error, message: 'unable to edit admin' });
         }
-        if (req.body.lastName) {
-            admin.lastName = req.body.lastName;
-        }
-        if (req.body.email) {
-            admin.email = req.body.email;
-        }
-        if (req.body.phoneNumber) {
-            admin.phoneNumber = req.body.phoneNumber;
-        }
-        if (req.body.whatsappNumber) {
-            admin.whatsappNumber = req.body.whatsappNumber;
-        }
-        if (req.body.userName) {
-            admin.userName = req.body.userName;
-        }
-        if (req.body.password) {
-            admin.password = req.body.password;
-        }
-    } catch (error) {
-        console.log('error', error);
-        res.status(error.status);
-        res.send({ error, message: 'unable to edit admin' });
+    } else {
+        res.status(401).send({
+            message: 'Not authorized, only a super admin can edit an admin',
+        });
     }
 };
 
 exports.delete = async (req, res) => {
-    try {
-        const admin = await Admin.deleteOne({ _id: req.params.id });
-        res.status(204).send({ message: 'deleted', admin });
-    } catch (error) {
-        console.log('error', error);
-        res.status(error.status);
-        res.send({ error, message: 'unable to delete admin' });
+    const userRole = decodeAuthToken.role;
+
+    if (userRole === 'superAdmin') {
+        try {
+            const admin = await Admin.deleteOne({ _id: req.params.id });
+            res.status(204).send({ message: 'deleted', admin });
+        } catch (error) {
+            console.log('error', error);
+            res.status(error.status);
+            res.send({ error, message: 'unable to delete admin' });
+        }
+    } else {
+        res.status(401).send({
+            message: 'Not authorized, only a super admin can delete an admin',
+        });
     }
 };
