fc97c34896c26557cf9af7b03b16e012c13be5a8
BisiDavi <oludavidconnect@gmail.com>
2021-07-30 00:52:47 +0100

    main

--- utils/validateToken.js
+++ utils/validateToken.js
@@ -2,9 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     validateToken: (req, res, next) => {
-        const authorizationHeader = req.headers?.authorization;
+        const authorizationHeader = req.headers.authorization;
+        console.log('authorizationHeader', authorizationHeader);
         let result;
-        if (authorizationHeader) {
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.split(' ')[0] === 'Bearer'
+        ) {
             const token = req.headers.authorization.split(' ')[1];
             const options = {
                 expiresIn: '2d',
